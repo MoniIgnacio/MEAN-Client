e271b6923bf3e18360204ec11c1df0e9d7c1a7c7
MoniIgnacio <moniignacio02@gmail.com>
2022-12-07 19:23:59 +0400

    add editProduct method done.

--- src/app/components/crear-producto/crear-producto.component.ts
+++ src/app/components/crear-producto/crear-producto.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, Validators, FormBuilder } from '@angular/forms';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { Producto } from 'src/app/models/producto';
+import { ProductoService } from 'src/app/services/producto.service';
 
 @Component({
   selector: 'app-crear-producto',
@@ -11,19 +12,25 @@ import { Producto } from 'src/app/models/producto';
 })
 export class CrearProductoComponent implements OnInit {
   productoForm: FormGroup;
+  titulo = 'Crear producto';
+  id: string | null;
 
   constructor (private fb: FormBuilder,
                private router: Router,
-               private toastr: ToastrService) {
+               private toastr: ToastrService,
+               private _productoService: ProductoService,
+               private aRouter: ActivatedRoute) {
     this.productoForm = this.fb.group({
       producto: ['', Validators.required],
       categoria: ['', Validators.required],
       ubicacion: ['', Validators.required],
       precio: ['', Validators.required]
     })
+    this.id = this.aRouter.snapshot.paramMap.get('id');
   }
 
   ngOnInit(): void {
+    this.esEditar()
 
   }
 
@@ -36,8 +43,40 @@ export class CrearProductoComponent implements OnInit {
       ubicacion:this.productoForm.get('ubicacion')?.value,
       precio:this.productoForm.get('precio')?.value,
     }
-    console.log(PRODUCTO)
-    this.toastr.success('Registrado correctamente el producto', 'Producto Creado!')
-    this.router.navigate(['/']);
+    
+    if(this.id !== null){
+      // editar producto
+      this._productoService.editarProducto(this.id, PRODUCTO).subscribe(data =>{ 
+        this.toastr.info('El producto fue editado correctamente', 'Producto actualizado!')
+        this.router.navigate(['/']);
+      }, error => {
+        console.log(error);
+        this.productoForm.reset();
+      })
+    }else{
+      // agregar producto
+      this._productoService.crearProducto(PRODUCTO).subscribe(data => {
+        this.toastr.success('Registrado correctamente el producto', 'Producto Creado!')
+        this.router.navigate(['/']);
+      }, error => {
+        console.log(error);
+        this.productoForm.reset();
+      })
+    }
+    }
+
+
+  esEditar() {
+    if(this.id !== null) {
+      this.titulo = 'Editar producto';
+      this._productoService.obtenerProducto(this.id).subscribe(data => {
+        this.productoForm.setValue({
+          producto: data.nombre,
+          categoria: data.categoria,
+          ubicacion: data.ubicacion,
+          precio: data.precio
+        })
+      })
+    }
   }
 }
--- src/app/services/producto.service.ts
+++ src/app/services/producto.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { Producto } from '../models/producto';
 
 @Injectable({
   providedIn: 'root'
@@ -15,5 +16,17 @@ url = 'http://localhost:4000/api/productos/'
 
   eliminarProducto(productoId: string): Observable<any>{
     return this.http.delete(this.url + productoId)
+  }
+
+  crearProducto(producto: Producto): Observable<any>{
+    return this.http.post(this.url, producto)
+  }
+
+  obtenerProducto(productoId: string): Observable<any>{
+    return this.http.get(this.url + productoId)
+  }
+
+  editarProducto(productoId: string, producto: Producto): Observable<any>{
+    return this.http.put(this.url + productoId, producto)
   }
 }
